fa56c436a4fdb30dad42676425731284a83a96dc
Ramtin Gharleghi <gharleghi@gmail.com>
2022-04-14 01:59:29 +1000

    OSX compilation patch, testing fixes and cleanup

--- recipe/test.sh
+++ recipe/test.sh
@@ -1,3 +1,9 @@
 set -e
 git clone --quiet https://github.com/vmtk/vmtk-test-data.git ./build/tests/vmtk-test-data
-xvfb-run --auto-servernum pytest ./build/tests/
+if [ "$(uname)" == "Darwin" ]; then
+    pytest ./build/tests/   
+elif [ "$(uname)"  = "Linux" ]; then
+    xvfb-run --auto-servernum pytest ./build/tests/
+else
+	exit 1
+fi
