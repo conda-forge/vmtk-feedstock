69216d713beb892a05d2ef8b0326433a334f1233
Ramtin Gharleghi <gharleghi@gmail.com>
2022-04-14 17:53:50 +1000

    add timeout to stop hanging tests stalling the whole build

--- recipe/test.sh
+++ recipe/test.sh
@@ -1,9 +1,9 @@
 set -e
 git clone --quiet https://github.com/vmtk/vmtk-test-data.git ./build/tests/vmtk-test-data
 if [ "$(uname)" == "Darwin" ]; then
-    pytest ./build/tests/   
+    pytest --timeout=1200 ./build/tests/   
 elif [ "$(uname)"  = "Linux" ]; then
-    xvfb-run --auto-servernum pytest ./build/tests/
+    xvfb-run --auto-servernum pytest --timeout=1200 ./build/tests/
 else
 	exit 1
 fi
